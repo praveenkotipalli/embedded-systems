31951ae67b0571c87f3889e9d7ac9d4a4fe16608
KOTIPALLI PRAVEEN KUMAR <2200032132@kluniversity.in>
2026-02-26 10:24:07 +0530

    first commit

---
+++ blink1.ino
@@ -0,0 +1,15 @@
+
+void setup()
+{
+  pinMode(13, OUTPUT);//13 or LED_BUILTIN
+}
+
+void loop()
+{
+  // turn the LED on (HIGH is the voltage level)
+  digitalWrite(13, HIGH);
+  delay(1000); // Wait for 1000 millisecond(s)
+  // turn the LED off by making the voltage LOW
+  digitalWrite(LED_BUILTIN, LOW);
+  delay(1000); // Wait for 1000 millisecond(s)
+}
