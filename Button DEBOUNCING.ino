737172d61c06b33637f13fdf0933d4f43203202f
KOTIPALLI PRAVEEN KUMAR <2200032132@kluniversity.in>
2026-03-01 19:55:36 +0530

    added Led toggle with Button DEBOUNCING

--- Button DEBOUNCING.ino
+++ Button DEBOUNCING.ino
@@ -1,33 +1,41 @@
-const int LED=9; 
-const int BUTTON=2; 
 
-boolean lastButton = LOW;
-boolean currentButton = LOW;
-boolean ledOn = false;
+const int LED=9;             // The LED is connected to pin 9
+const int BUTTON=2;          // The Button is connected to pin 2
+boolean lastButton = LOW;    // Variable containing the previous button state
+boolean currentButton = LOW; // Variable containing the current button state
+boolean ledOn = false;       // The present state of the LED (on/off)
 
 void setup()
 {
- pinMode (LED, OUTPUT); // Set the LED pin as an output
- pinMode (BUTTON, INPUT); // Set button as input (not required)
+  pinMode (LED, OUTPUT);     // Set the LED pin as an output
+  pinMode (BUTTON, INPUT);   // Set button as input (not required)
 }
 
-
-boolean debounce(boolean last){
-  boolean current = digitalRead(BUTTON);
-  if(last != current){
-   delay(5);
-    current = digitalRead(BUTTON);
+/*
+* Debouncing Function
+* Pass it the previous button state,
+* and get back the current debounced button state.
+*/
+boolean debounce(boolean last)
+{
+  boolean current = digitalRead(BUTTON);       // Read the button state
+  if (last != current)                         // If it's different…
+  {
+    delay(5);                                  // Wait 5ms
+    current = digitalRead(BUTTON);             // Read it again
   }
-  return current;
+  return current;                              // Return the current value
 }
-
 
 void loop()
 {
-  currentButton = debounce(lastButton);
-  if(lastButton == LOW && currentButton == HIGH){
-   	ledOn = !ledOn; 
+  currentButton = debounce(lastButton);              // Read debounced state
+  if (lastButton == LOW && currentButton == HIGH)    // If it was pressed…
+  {
+    ledOn = !ledOn;                                  // Toggle the LED value
   }
-  lastButton = currentButton;
-  digitalWrite(LED, ledOn);
+  lastButton = currentButton;                        // Reset button value
+  
+  digitalWrite(LED, ledOn);                          // Change the LED state
+
 }
