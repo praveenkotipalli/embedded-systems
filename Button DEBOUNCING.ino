125846ad5c90fea4e713e9d160bb919283bd1364
KOTIPALLI PRAVEEN KUMAR <2200032132@kluniversity.in>
2026-03-01 19:42:07 +0530

    added Led with Button DEBOUNCING

---
+++ Button DEBOUNCING.ino
@@ -0,0 +1,33 @@
+const int LED=9; 
+const int BUTTON=2; 
+
+boolean lastButton = LOW;
+boolean currentButton = LOW;
+boolean ledOn = false;
+
+void setup()
+{
+ pinMode (LED, OUTPUT); // Set the LED pin as an output
+ pinMode (BUTTON, INPUT); // Set button as input (not required)
+}
+
+
+boolean debounce(boolean last){
+  boolean current = digitalRead(BUTTON);
+  if(last != current){
+   delay(5);
+    current = digitalRead(BUTTON);
+  }
+  return current;
+}
+
+
+void loop()
+{
+  currentButton = debounce(lastButton);
+  if(lastButton == LOW && currentButton == HIGH){
+   	ledOn = !ledOn; 
+  }
+  lastButton = currentButton;
+  digitalWrite(LED, ledOn);
+}
